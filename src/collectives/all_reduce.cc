92f13dc2a44a43b553ae8e4d868013179a6a3ee7
Luca Wehrstedt <lcw@fb.com>
2020-08-07 09:04:23 -0700

    Remove even more, especially device kernels

--- src/collectives/all_reduce.cc
+++
@@ -1,7 +0,0 @@
-/*************************************************************************
- * Copyright (c) 2015-2019, NVIDIA CORPORATION. All rights reserved.
- *
- * See LICENSE.txt for license information
- ************************************************************************/
-
-#include "enqueue.h"
--- src/collectives/device/reduce.h
+++
@@ -1,9 +0,0 @@
-/*************************************************************************
- * Copyright (c) 2015-2020, NVIDIA CORPORATION. All rights reserved.
- *
- * See LICENSE.txt for license information
- ************************************************************************/
-
-#include "devcomm.h"
-#include "primitives.h"
-#include "collectives.h"
